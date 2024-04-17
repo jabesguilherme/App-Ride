696d1627960e1015a5d088046b24d48b80426977
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-04-17 00:24:54 -0300

    Introduzindo style list

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const rideListElement = document.querySelector("#rideList")
 const allRides = getAllRides()
 
-    allRides.forEach(async ([id, value]) => {
+allRides.forEach(async ([id, value]) => {
 
     const ride = JSON.parse(value)
     ride.id = id
@@ -13,19 +13,44 @@ const allRides = getAllRides()
 
     const itemElement = document.createElement("li")
     itemElement.id = ride.id
+    itemElement.className = "d-flex p-1 align-items-center justify-content-between shadow-sm gap-3"
+
+    const mapElement = document.createElement("div")
+    mapElement.style="width:100px;height:100px"
+    mapElement.classList.add("bg-secondary")
+    mapElement.classList.add("rounded-4")
+
+    const dataElement = document.createElement("div")
+    dataElement.className = "flex-fill d-flex flex-column"
 
     const cityDiv = document.createElement("div")
     cityDiv.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`
+    cityDiv.className = "text-primary mb-2"
 
     const maxSpeedDiv = document.createElement("div")
     maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`
+    maxSpeedDiv.className = "h5"
 
     const distanceDiv = document.createElement("div")
     distanceDiv.innerText = `Distance: ${getDistance(ride.data)} Km`
 
-    itemElement.appendChild(cityDiv)
-    itemElement.appendChild(maxSpeedDiv)
-    itemElement.appendChild(distanceDiv)
+    const durationDiv = document.createElement("div")
+    durationDiv.innerText = `Duration: ${getDuration(ride)}`
+
+    const dateDiv = document.createElement("div")
+    dateDiv.innerText = getStartDate(ride)
+    dateDiv.className = "text-secondary mt-2"
+
+
+    dataElement.appendChild(cityDiv)
+    dataElement.appendChild(maxSpeedDiv)
+    dataElement.appendChild(distanceDiv)
+    dataElement.appendChild(durationDiv)
+    dataElement.appendChild(dateDiv)
+
+    itemElement.appendChild(mapElement)
+    itemElement.appendChild(dataElement)
+
     rideListElement.appendChild(itemElement)
 
 })
@@ -64,14 +89,14 @@ function getDistance(positions) {
         const deltaLatitude = toRad(p2.latitude - p1.latitude)
         const deltaLongitude = toRad(p2.longitude - p1.longitude)
 
-        const a = Math.sin(deltaLatitude/2) * 
-            Math.sin(deltaLatitude/2) + 
-            Math.sin(deltaLongitude/2) * 
-            Math.sin(deltaLongitude/2) * 
-            Math.cos(toRad (p1.latitude)) * 
+        const a = Math.sin(deltaLatitude / 2) *
+            Math.sin(deltaLatitude / 2) +
+            Math.sin(deltaLongitude / 2) *
+            Math.sin(deltaLongitude / 2) *
+            Math.cos(toRad(p1.latitude)) *
             Math.cos(toRad(p2.latitude))
 
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
         const distance = earthRadiusKm * c
 
         totalDistance += distance
@@ -80,6 +105,34 @@ function getDistance(positions) {
         return degree * Math.PI / 180
     }
     return totalDistance.toFixed(2)
+}
+
+function getDuration(ride) {
+
+    function format(number, digits) {
+        return String(number.toFixed(0)).padStart(2, '0')
+    }
+
+    const interval = (ride.stopTime - ride.startTime) / 1000
+
+    const minutes = Math.trunc(interval / 60)
+    const seconds = interval % 60
+
+    return `${format(minutes, 2)}:${format(seconds, 2)}`
+}
+
+function getStartDate(ride) {
+    const d = new Date(ride.startTime)
+
+    const day = d.toLocaleString("pt-BR", { day: "numeric" })
+    const month = d.toLocaleString("pt-BR", { month: "long" })
+    const year = d.toLocaleString("pt-BR", { year: "numeric" })
+    const hour = d.toLocaleString("pt-BR", { hour: "2-digit" })
+    const minute = d.toLocaleString("pt-BR", { minute: "2-digit" })
+
+
+    return `${hour}:${minute} - ${day}, ${month} de ${year}`
+
 }
 
 
